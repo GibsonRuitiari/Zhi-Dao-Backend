2708c147d5e3787554530ef9ea30e085b8f56d30
GibsonRuitiari <gibsonruitiari@gmail.com>
2022-06-03 15:43:45 +0300

    removed unnecesary dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,9 +16,7 @@ repositories {
 dependencies {
     implementation("it.skrape:skrapeit:1.2.1")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.1")
-   implementation("it.skrape:skrapeit-browser-fetcher:1.1.5")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.3")
-    implementation ("org.jsoup:jsoup:1.15.1")
     testImplementation(kotlin("test"))
     testImplementation("com.google.truth:truth:1.1.3")
     implementation(kotlin("reflect"))
